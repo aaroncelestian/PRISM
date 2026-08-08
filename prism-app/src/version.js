9efffff2e74a570f283845d498dd515566c5e099
Aaron Celestian <aaron.celestian@gmail.com>
2026-08-08 09:38:24 -0700

    version up

--- prism-app/src/version.js
+++ prism-app/src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "1.2.1";
+export const APP_VERSION = "1.2.2";
 
 // Bump COLLECTION_SCHEMA when the shape of a collection record changes.
 // Old saved files will be migrated up via migrateCollectionRecord().
